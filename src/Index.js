725118b5cfc098a1c68aac59b814ce596d616af3
GiovanniSierraCun <fredy_sierra@cun.edu.co>
2025-09-15 19:36:42 -0500

    FUNCIONES

--- src/Index.js
+++ src/Index.js
@@ -1,9 +1,12 @@
 import ReactDom from "react-dom/client"
+import IndexApp from "./IndexApp";
+
 
 const root =ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
 
+    <IndexApp/>
    
 
 );
--- src/IndexApp.jsx
+++ src/IndexApp.jsx
@@ -2,11 +2,24 @@
 
 const IndexApp=()=>{
 
+    const user="andres"
+
     return(
 
+        <>
+
+        <nav></nav>
+
+        <body></body>
+            <h1 className="saludo">Bienvenido {user}</h1>
         
+        </>
+
 
 
     );
 
+
 }
+
+export default IndexApp;
